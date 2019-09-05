5b8695b4b83fa15c026c14eda4bc1b4ff3cbbfb6
Bruno Conde Kind <brunocondekind@hotmail.com>
2019-09-05 13:05:37 -0300

    cleaned some stuff, no big changes

---
+++ auxinfo.py
@@ -0,0 +1,18 @@
+#!python
+
+colnamestranslation = {
+\
+# used
+'codigo': 'code',
+'fechamento_atual': 'close',
+'volume_financeiro': 'volume',
+'datahora': 'datetime',
+\
+#unused
+'abertura': 'open',
+'maximo': 'max',
+'minimo': 'min',
+'fechamento_anterior': 'prevclose',
+'negocios': 'buisiness',  # ???
+'quantidade_papeis': 'stock_cnt'  # ???
+}
